d1405a20b143aa60e380e94055c020c0ce979230
Rogelio Gudino <cananito@gmail.com>
2026-03-03 11:11:05 -0800

    Adjust

--- setup-scripts/linux-personal/install-st.sh
+++ setup-scripts/linux-personal/install-st.sh
@@ -1,10 +1,4 @@
 CURR_LOCATION="$PWD"
-cd "$HOME"/dotfiles/tars
-if [ ! -f "$HOME"/dotfiles/tars/st-0.9.3.tar.gz ]; then
-  curl https://dl.suckless.org/st/st-0.9.3.tar.gz -OL
-fi
-tar -xzf st-0.9.3.tar.gz
-cd "$HOME"/dotfiles/tars/st-0.9.3
-sudo make install
-rm -rf "$HOME"/dotfiles/tars/st-0.9.3
+cd "$HOME"/dotfiles/st
+sudo make clean install
 cd "$CURR_LOCATION"
