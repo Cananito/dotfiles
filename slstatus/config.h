e0593d7fccdaa93cb0af79ee26ed153a0984415a
Rogelio Gudino <cananito@gmail.com>
2026-03-19 15:16:35 -0700

    Status, pending volume

--- slstatus/config.h
+++ slstatus/config.h
@@ -65,6 +65,10 @@ static const char unknown_str[] = "n/a";
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  */
 static const struct arg args[] = {
-	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	/* function, format, argument */
+  { wifi_essid, " WiFi: %s ", "wlp2s0"},
+  { wifi_perc, "%s%% |", "wlp2s0"},
+  // { vol_perc, " Vol: %s%% |", NULL},
+  { battery_perc, " Bat: %s%% |", "BAT0" },
+  { datetime, " %s ", "%a %b %d  %I:%M %p" },
 };
