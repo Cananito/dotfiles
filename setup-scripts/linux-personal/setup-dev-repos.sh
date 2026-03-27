286220ff6115f173116119902e8bbc1526f19d58
Rogelio Gudino <cananito@gmail.com>
2026-03-27 15:18:14 -0700

    Correct way to check-out website repo.

--- setup-scripts/linux-personal/setup-dev-repos.sh
+++ setup-scripts/linux-personal/setup-dev-repos.sh
@@ -2,7 +2,7 @@ CURR_LOCATION="$PWD"
 cd "$HOME"/repos
 # TODO: Automate authentication.
 git clone https://github.com/Cananito/notebooks.git
-git clone https://github.com/Cananito/Cananito.github.io.git
+git clone https://github.com/Cananito/Cananito.github.io.git --recurse-submodules
 git clone https://github.com/Cananito/c_dev.git
 git clone https://github.com/Cananito/barbellcalc.git
 cd "$CURR_LOCATION"
