10fe2d5d1d6959833e95b01af795fdc2fdbcd406
Rogelio Gudino <cananito@gmail.com>
2026-06-23 14:37:25 -0700

    Sim screenshots location.

--- setup-scripts/mac-personal/setup-mac-settings.sh
+++ setup-scripts/mac-personal/setup-mac-settings.sh
@@ -1,2 +1,3 @@
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write -g NSDisableAutomaticTermination -bool true
+defaults write com.apple.iphonesimulator "ScreenShotSaveLocation" -string "~/Downloads"
