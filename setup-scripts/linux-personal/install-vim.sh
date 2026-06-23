dce0297259149fcc08d03cba7ecfcac13aa1f0d2
Rogelio Gudino <cananito@gmail.com>
2026-06-23 16:01:55 -0700

    Changes git insallation to a specific commit of the git repo.

--- setup-scripts/linux-personal/install-vim.sh
+++ setup-scripts/linux-personal/install-vim.sh
@@ -1,11 +1,16 @@
 # Installing from source so it has +clipboard support.
 CURR_LOCATION="$PWD"
 cd "$HOME"/dotfiles/tars
-if [ ! -f "$HOME"/dotfiles/tars/vim-9.2.tar.bz2 ]; then
-  curl https://www.vim.org/downloads/vim-9.2.tar.bz2 -OL
+# if [ ! -f "$HOME"/dotfiles/tars/vim-9.2.tar.bz2 ]; then
+  # curl https://www.vim.org/downloads/vim-9.2.tar.bz2 -OL
+# fi
+# tar -xjf vim-9.2.tar.bz2
+# cd "$HOME"/dotfiles/tars/vim92
+if [ ! -f "$HOME"/dotfiles/tars/vim-5c1b989b4aabf1549910752dcfb44030e64edfcc.tar.gz ]; then
+  curl https://github.com/vim/vim/archive/5c1b989b4aabf1549910752dcfb44030e64edfcc.tar.gz -OL
 fi
-tar -xjf vim-9.2.tar.bz2
-cd "$HOME"/dotfiles/tars/vim92
+tar -xzf vim-5c1b989b4aabf1549910752dcfb44030e64edfcc.tar.gz
+cd "$HOME"/dotfiles/tars/vim-5c1b989b4aabf1549910752dcfb44030e64edfcc
 ./configure --with-x --enable-gui=no
 make
 sudo make install
